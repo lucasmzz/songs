cee6506bf9b9736a46fda294fafc6e2f8cff99c1
Lucas Mazzutti <lucas.mazzutti@booking.com>
2019-05-10 16:25:34 +0200

    final commit

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,20 +1,17 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {connect} from 'react-redux';
 
-class SongDetail extends Component {
-
-	render(){
-		return(
-			(this.props.selectedSong) ? 
+const SongDetail = ({ selectedSong }) => {
+	return(
+			(selectedSong) ? 
 				<div className="ui segment">
-					<h4>Title: {this.props.selectedSong.title}</h4>
-					<p>Duration: {this.props.selectedSong.length}</p>
+					<h4>Title: {selectedSong.title}</h4>
+					<p>Duration: {selectedSong.length}</p>
 
 				</div> 
 			: 
 				<div>select a song...</div>
-		);
-	};
+	);
 }
 
 const mapStateToProps = (state) => {
